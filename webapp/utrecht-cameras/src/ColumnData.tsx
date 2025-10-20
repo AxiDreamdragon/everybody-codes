b889c1642211c2342403f9e7ed2af18ee2ed4443
Axi <axidreamdragon@gmail.com>
2025-10-20 12:14:10 +0200

    Organize ColumnData in table

--- webapp/utrecht-cameras/src/ColumnData.tsx
+++ webapp/utrecht-cameras/src/ColumnData.tsx
@@ -5,41 +5,114 @@ type Props = {
 }
 
 function ColumnData({ orderedCameraData }: Props) {
-	return (<div className='camera-columns'>
-		<div>
-			Kolom 1 (minder dan 600)
-			{/* iterating over each camera four times is slow, but the database is small enough that it's alright. */}
-			<ul>
+	{/* iterating over each camera four times is slow, but the database is small enough that it's alright. */ }
+	return (
+		<div className='camera-columns'>
+			<div>
+				Camera's tot 600
 				{orderedCameraData[0].length > 0 ?
-					orderedCameraData[0].map(camera => <li>{camera.name}</li>) :
+					<table>
+						<thead>
+							<tr>
+								<th>Nummer</th>
+								<th>Naam</th>
+								<th>Latitude</th>
+								<th>Longitude</th>
+							</tr>
+						</thead>
+						<tbody>
+							{orderedCameraData[0].map(camera => (
+								<tr>
+									<td>{camera.id}</td>
+									<td>{camera.name}</td>
+									<td>{camera.lat}</td>
+									<td>{camera.lng}</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+					:
 					<p>Geen camera's gevonden in dit bereik.</p>}
-			</ul>
-		</div>
-		<div>
-			Kolom 2 (600-700)
-			<ul>
+			</div>
+			<div>
+				Camera's 600 tot 700
 				{orderedCameraData[1].length > 0 ?
-					orderedCameraData[1].map(camera => <li>{camera.name}</li>) :
+					<table>
+						<thead>
+							<tr>
+								<th>Nummer</th>
+								<th>Naam</th>
+								<th>Latitude</th>
+								<th>Longitude</th>
+							</tr>
+						</thead>
+						<tbody>
+							{orderedCameraData[1].map(camera => (
+								<tr>
+									<td>{camera.id}</td>
+									<td>{camera.name}</td>
+									<td>{camera.lat}</td>
+									<td>{camera.lng}</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+					:
 					<p>Geen camera's gevonden in dit bereik.</p>}
-			</ul>
-		</div>
-		<div>
-			Kolom 3 (700-800)
-			<ul>
+			</div>
+			<div>
+				Camera's 700 tot 800
 				{orderedCameraData[2].length > 0 ?
-					orderedCameraData[2].map(camera => <li>{camera.name}</li>) :
+					<table>
+						<thead>
+							<tr>
+								<th>Nummer</th>
+								<th>Naam</th>
+								<th>Latitude</th>
+								<th>Longitude</th>
+							</tr>
+						</thead>
+						<tbody>
+							{orderedCameraData[2].map(camera => (
+								<tr>
+									<td>{camera.id}</td>
+									<td>{camera.name}</td>
+									<td>{camera.lat}</td>
+									<td>{camera.lng}</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+					:
 					<p>Geen camera's gevonden in dit bereik.</p>}
-			</ul>
-		</div>
-		<div>
-			Kolom 4 (overig)
-			<ul>
+			</div>
+			<div>
+				Camera's (overigen)
 				{orderedCameraData[3].length > 0 ?
-					orderedCameraData[3].map(camera => <li>{camera.name}</li>) :
+					<table>
+						<thead>
+							<tr>
+								<th>Nummer</th>
+								<th>Naam</th>
+								<th>Latitude</th>
+								<th>Longitude</th>
+							</tr>
+						</thead>
+						<tbody>
+							{orderedCameraData[3].map(camera => (
+								<tr>
+									<td>{camera.id}</td>
+									<td>{camera.name}</td>
+									<td>{camera.lat}</td>
+									<td>{camera.lng}</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+					:
 					<p>Geen camera's gevonden in dit bereik.</p>}
-			</ul>
-		</div>
-	</div>);
+			</div>
+		</div>);
 }
 
 export default ColumnData;
