43066a800ec127ef1dc745e15e9100ddb4c5ac41
Axi <axidreamdragon@gmail.com>
2025-10-20 14:46:27 +0200

    Add try-catch to data fetching

--- webapp/utrecht-cameras/src/App.tsx
+++ webapp/utrecht-cameras/src/App.tsx
@@ -12,35 +12,41 @@ function App() {
 
   useEffect(() => {
     const fetchCameraData = async () => {
-      const response = await fetch('/api/search.php');
-      const rawData = await response.json();
+      try {
+        const response = await fetch('/api/search.php');
+        const rawData = await response.json();
 
-      //I feel like this could be cleaner but it works - if I have time left I can look at it in detail
-      setOrderedCameraData(() => {
-        const newData: Camera[][] = [[], [], [], []];
+        //I feel like this could be cleaner but it works - if I have time left I can look at it in detail
+        setOrderedCameraData(() => {
+          const newData: Camera[][] = [[], [], [], []];
 
-        for (const camera of rawData) {
-          const typedCamera: Camera =
-          {
-            id: camera[0],
-            name: camera[1],
-            lat: camera[2],
-            lng: camera[3]
-          };
+          for (const camera of rawData) {
+            const typedCamera: Camera =
+            {
+              id: camera[0],
+              name: camera[1],
+              lat: camera[2],
+              lng: camera[3]
+            };
 
-          if (typedCamera.id < 600) {
-            newData[0].push(typedCamera);
-          } else if (typedCamera.id < 700) {
-            newData[1].push(typedCamera);
-          } else if (typedCamera.id < 800) {
-            newData[2].push(typedCamera);
-          } else {
-            newData[3].push(typedCamera);
+            if (typedCamera.id < 600) {
+              newData[0].push(typedCamera);
+            } else if (typedCamera.id < 700) {
+              newData[1].push(typedCamera);
+            } else if (typedCamera.id < 800) {
+              newData[2].push(typedCamera);
+            } else {
+              newData[3].push(typedCamera);
+            }
           }
-        }
 
-        return newData;
-      });
+          return newData;
+        });
+      } catch (e) {
+        if (e instanceof Error) {
+          console.error(e.message);
+        }
+      }
     }
 
     fetchCameraData();
