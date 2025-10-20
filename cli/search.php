cd8f9f6cd411859afe9f99be14b0d89e66f6bb8d
Axi <axidreamdragon@gmail.com>
2025-10-20 11:07:50 +0200

    Create CLI script
    Currently doesn't use a named parameter, though

---
+++ cli/search.php
@@ -0,0 +1,30 @@
+<?php
+
+// check argv[1] for now - can check --name later
+if ($argc < 2) {
+	echo "Usage: php search.php <query>\n";
+	exit(1);
+}
+
+$query = $argv[1];
+
+// read CSV
+$file = fopen(__DIR__ . "/cameras-defb.csv", "r");
+fgetcsv($file, escape: "\\"); //skip first line, this contains headers
+
+while ($row = fgetcsv($file, escape: "\\")) {
+	$camera = str_getcsv($row[0], ';', '"', '\\');
+	preg_match('/UTR-CM-(\d+)(\D.+)/', $camera[0], $matches);
+	if (!isset($matches[1]) || !isset($matches[2])) {
+		//Invalid camera
+		continue;
+	}
+
+	$cam_id = $matches[1];
+	$cam_name = $matches[2];
+
+	if (str_contains($cam_name, $query)) {
+		array_unshift($camera, $matches[1]);
+		echo implode(' | ', $camera) . "\n";
+	}
+}
