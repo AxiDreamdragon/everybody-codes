b8481a67cec6571439fa829b8080fa35c8ae3d73
Axi <axidreamdragon@gmail.com>
2025-10-20 12:05:50 +0200

    Create camera map

--- webapp/utrecht-cameras/src/App.tsx
+++ webapp/utrecht-cameras/src/App.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import type { Camera } from './Camera';
+import ColumnData from './ColumnData';
+import CameraMap from './CameraMap';
 
 function App() {
   const [orderedCameraData, setOrderedCameraData] = useState<Camera[][]>([[], [], [], []]);
@@ -48,41 +50,7 @@ function App() {
         <h1>Utrecht Cameras</h1>
       </nav>
       <div>
-      </div>
-      <div className='camera-columns'>
-        <div>
-          Kolom 1 (minder dan 600)
-          {/* iterating over each camera four times is slow, but the database is small enough that it's alright. */}
-          <ul>
-            {orderedCameraData[0].length > 0 ?
-              orderedCameraData[0].map(camera => <li>{camera.name}</li>) :
-              <p>Geen camera's gevonden in dit bereik.</p>}
-          </ul>
-        </div>
-        <div>
-          Kolom 2 (600-700)
-          <ul>
-            {orderedCameraData[1].length > 0 ?
-              orderedCameraData[1].map(camera => <li>{camera.name}</li>) :
-              <p>Geen camera's gevonden in dit bereik.</p>}
-          </ul>
-        </div>
-        <div>
-          Kolom 3 (700-800)
-          <ul>
-            {orderedCameraData[2].length > 0 ?
-              orderedCameraData[2].map(camera => <li>{camera.name}</li>) :
-              <p>Geen camera's gevonden in dit bereik.</p>}
-          </ul>
-        </div>
-        <div>
-          Kolom 4 (overig)
-          <ul>
-            {orderedCameraData[3].length > 0 ?
-              orderedCameraData[3].map(camera => <li>{camera.name}</li>) :
-              <p>Geen camera's gevonden in dit bereik.</p>}
-          </ul>
-        </div>
+        <CameraMap cameras={orderedCameraData.flat()} />
       </div>
     </div>
   )
---
+++ webapp/utrecht-cameras/src/CameraMap.tsx
@@ -0,0 +1,34 @@
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import type { Camera } from "./Camera";
+import { LatLng } from "leaflet";
+import 'leaflet/dist/leaflet.css';
+
+type Props = {
+	cameras: Camera[];
+}
+
+function CameraMap({ cameras }: Props) {
+	const position = new LatLng(52.0914, 5.1115);
+
+	return (
+		<MapContainer style={{ width: '100dvw', height: '100dvh' }}
+			center={position}
+			zoom={13}>
+			<TileLayer
+				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+			/>
+			<TileLayer
+				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+			/>
+			{cameras.map(camera => <Marker position={new LatLng(camera.lat, camera.lng)}>
+				<Popup>
+					{camera.name}
+				</Popup>
+			</Marker>)}
+		</MapContainer>
+	)
+}
+
+export default CameraMap;
